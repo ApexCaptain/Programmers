631b1ad9f288c71273dcf8da07b0df84c2e84d51
ApexCaptain <ayteneve93@gmail.com>
2021-08-09 13:13:40 +0900

    RegCommit 2021-08-09

---
+++ week2/solution.js
@@ -0,0 +1,55 @@
+function solution(scores) {
+    const students = new Array(scores.length)
+    scores.forEach((values, grader) => {
+        values.forEach((score, student) => {
+            if(!students[student]) students[student] = new Array()
+            students[student].push(score)
+        })
+    })
+    return students.map((scores, index) => {
+        let maxValue = -1
+        let isMaxDuplicated = false
+
+        let minValue = 101
+        let isMinDuplicated = false
+
+        let sum = 0
+        let count = scores.length
+
+        scores.forEach(eachScore => {
+            sum += eachScore
+
+            if(eachScore == minValue) isMinDuplicated = true
+            else if(eachScore < minValue) {
+                isMinDuplicated = false
+                minValue = eachScore
+            }
+            if(eachScore == maxValue) isMaxDuplicated = true
+            else if(eachScore > maxValue) {
+                isMaxDuplicated = false
+                maxValue = eachScore
+            }
+        })
+        if(!isMaxDuplicated && scores[index] == maxValue) {
+            sum -= maxValue
+            count--
+        }
+        if(!isMinDuplicated && scores[index] == minValue) {
+            sum -= minValue
+            count--
+        }
+        let average = sum / count
+        if(average >= 90) return 'A'
+        else if(average >= 80) return 'B'
+        else if(average >= 70) return 'C'
+        else if(average >= 50) return 'D'
+        else return 'F'
+    }).join("")
+}
+
+const r = solution(
+    [[70,49,90],
+    [68,50,38],
+    [73,31,100]]
+)
+console.log(r)
